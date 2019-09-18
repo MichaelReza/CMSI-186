5b9335055e9305712a63266b894c709e5ad902a6
MichaelReza <45471734+MichaelReza@users.noreply.github.com>
2019-09-18 12:35:11 -0700

    Add files via upload

--- assignment01/CountTheDays.java
+++ assignment01/CountTheDays.java
@@ -9,7 +9,7 @@ public class CountTheDays {
 
   // constructor
   public CountTheDays (String[] args) {
-    month1 = Long.parseLong(args[0]);
+    this.month1 = Long.parseLong(args[0]);
     this.day1 = Long.parseLong(args[1]);
     this.year1 = Long.parseLong(args[2]);
     this.month2 = Long.parseLong(args[3]);
@@ -20,5 +20,6 @@ public class CountTheDays {
   public static void main (String[] args) {
     CountTheDays ctd = new CountTheDays(args);
     long count = CalendarStuff.daysBetween(month1, day1, year1, month2, day2, year2);
+    System.out.println(count + " days between");
   }
 }
